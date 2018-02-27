cc04009cfda5b1ab2e37ab5f159f2a1b8c633739
mmademann <mjmademann@gmail.com>
2018-02-27 14:41:23 -0800

    fix linting error

--- src/index.js
+++ src/index.js
@@ -10,92 +10,92 @@ const AUDIO_CONTEXT = new audioContext();
 
 // returns the oscillator controls
 const Controls = (props) => (
-  <div>
-	  <div className="control-row">
-	  	Oscillator
-	    <button onClick={props.startOscillator}>Start</button>
-	    <button onClick={props.stopOscillator}>Stop</button>
-	  </div>
-	  <div className="control-row">
-	  	Waveform
-	    <button value="sine" onClick={props.waveType}>Sine</button>
-	    <button value="sawtooth" onClick={props.waveType}>Sawtooth</button>
-	    <button value="triangle" onClick={props.waveType}>Triangle</button>
-	    <button value="square" onClick={props.waveType}>Square</button>
-	  </div>
-	  <div className="control-row">
-	    	<p>Frequency:</p>
-	    	<input 
-	    		type="range" min="100" max="1450" 
-	    		defaultValue={props.frequency} 
-	    		onInput={props.changeFrequency}>
-	    	</input>
-	  </div>
-	  <div className="control-row">
-	      <p>Fine Tune:</p>
-	      <input 
-	      	type="range" min="-100" max="100" 
-	      	defaultValue="100" 
-	      	onInput={props.finetuneFrequency}>
-	      </input>
-	  </div>
+	<div>
+		<div className="control-row">
+			Oscillator
+			<button onClick={props.startOscillator}>Start</button>
+			<button onClick={props.stopOscillator}>Stop</button>
+		</div>
+		<div className="control-row">
+			Waveform
+			<button value="sine" onClick={props.waveType}>Sine</button>
+			<button value="sawtooth" onClick={props.waveType}>Sawtooth</button>
+			<button value="triangle" onClick={props.waveType}>Triangle</button>
+			<button value="square" onClick={props.waveType}>Square</button>
+		</div>
+		<div className="control-row">
+				<p>Frequency:</p>
+				<input 
+					type="range" min="100" max="1450" 
+					defaultValue={props.frequency} 
+					onInput={props.changeFrequency}>
+				</input>
+		</div>
+		<div className="control-row">
+				<p>Fine Tune:</p>
+				<input 
+					type="range" min="-100" max="100" 
+					defaultValue="100" 
+					onInput={props.finetuneFrequency}>
+				</input>
+		</div>
 	</div>
 );
 
 // oscillator component w/ audio controls
 class Oscillator extends React.Component {
 
-  oscillator = null;
+	oscillator = null;
 
-  // stops the oscillator if removed
-  componentWillUnmount() {
+	// stops the oscillator if removed
+	componentWillUnmount() {
 		if (this.oscillator) this.oscillator.stop();
-  };
+	};
 
-  // creates the oscillator
+	// creates the oscillator
 	startOscillator = () => {
-	  AUDIO_CONTEXT.resume();
-	  if (this.oscillator) this.oscillator.stop();
-	  this.oscillator = AUDIO_CONTEXT.createOscillator();
-	  this.oscillator.frequency.setValueAtTime(this.props.frequency, AUDIO_CONTEXT.currentTime);
-	  this.oscillator.connect(AUDIO_CONTEXT.destination);
-	  this.oscillator.start();
+		AUDIO_CONTEXT.resume();
+		if (this.oscillator) this.oscillator.stop();
+		this.oscillator = AUDIO_CONTEXT.createOscillator();
+		this.oscillator.frequency.setValueAtTime(this.props.frequency, AUDIO_CONTEXT.currentTime);
+		this.oscillator.connect(AUDIO_CONTEXT.destination);
+		this.oscillator.start();
 	};
 
 	// stops the oscillator
 	stopOscillator = () => {
-	  if (this.oscillator) this.oscillator.stop(); 
+		if (this.oscillator) this.oscillator.stop(); 
 	};
 
 	// changes the wave type of the oscillator 
 	waveType = (event) => {
-	  if (this.oscillator) this.oscillator.type = event.target.value;
+		if (this.oscillator) this.oscillator.type = event.target.value;
 	};
 
 	// slides through different frequencies
 	changeFrequency = (event) => {
-	  if (this.oscillator) this.oscillator.frequency.setValueAtTime(Math.pow(2, event.target.value / 100), AUDIO_CONTEXT.currentTime);
+		if (this.oscillator) this.oscillator.frequency.setValueAtTime(Math.pow(2, event.target.value / 100), AUDIO_CONTEXT.currentTime);
 	};
 
 	// fine tunes the frequency
 	finetuneFrequency = (event) => {
-	  if (this.oscillator) this.oscillator.detune.setValueAtTime(event.target.value, AUDIO_CONTEXT.currentTime);
+		if (this.oscillator) this.oscillator.detune.setValueAtTime(event.target.value, AUDIO_CONTEXT.currentTime);
 	};
 
-  render() {
-    return (
-    	<div className="oscillator-row">
-      	<Controls
-      		frequency={ this.props.frequency }
-      	  startOscillator={ this.startOscillator }
-      	  stopOscillator={ this.stopOscillator }
-      	  waveType={ this.waveType }
-      	  changeFrequency={ this.changeFrequency }
-      	  finetuneFrequency={ this.finetuneFrequency }
-      	/>
-	    </div>
-    );
-  }
+	render() {
+		return (
+			<div className="oscillator-row">
+				<Controls
+					frequency={ this.props.frequency }
+					startOscillator={ this.startOscillator }
+					stopOscillator={ this.stopOscillator }
+					waveType={ this.waveType }
+					changeFrequency={ this.changeFrequency }
+					finetuneFrequency={ this.finetuneFrequency }
+				/>
+			</div>
+		);
+	}
 }
 
 // main oscillator wrapper
@@ -116,6 +116,6 @@ const AudioContainer = (props) => {
 }
 
 ReactDOM.render(
-  <AudioContainer />,
-  document.getElementById('root') 
+	<AudioContainer />,
+	document.getElementById('root') 
 );
